f2aefae7c3263df62ce56f8f88a4fff72ca8a907
aritz <aritz.martinezh@gmail.com>
2019-11-08 14:08:52 +0100

    Select todo guapo

--- aritz 2/ejerPHP/reto2bbdd/SelectSubcategoria.php
+++ aritz 2/ejerPHP/reto2bbdd/SelectSubcategoria.php
@@ -1,6 +1,8 @@
 <?php
 function selectSubcategoria($dbh){
-    $stmt = $dbh->prepare("SELECT idSubcategoria FROM Subcategoria Where nomSubcategoria = 'motos'");
+    $stmt = $dbh->prepare("SELECT nomSubcategoria FROM Subcategoria 
+                           Where idSubcategoria = (SELECT idSubcategoria FROM Anuncio
+                                                   WHERE idAnuncio = 1)");
     $stmt->setFetchMode(PDO::FETCH_OBJ);
     $stmt->execute ();
 
